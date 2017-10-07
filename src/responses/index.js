4f12d1284c2c3f8d5d55d118fce2e9d0acbb10c8
ian <smith.ianrichard@gmail.com>
2017-10-07 13:05:31 -0500

    feature: wip facebook messenger list

--- src/responses/index.js
+++ src/responses/index.js
@@ -3,22 +3,52 @@ import resolvedQueryContains from '../utils/string-contains';
 import getGiphyUrl from '../utils/giphy-url';
 
 export default function getResponses() {
-
     const userProfile = getUserProfile();
 
     return {
         welcome: () => {
             return [
-                { text: `Heyo ${userProfile.firstNameWithSpace}- so I’ve been staring at your profile pic…` },
+                { text: `Hey ${userProfile.firstNameWithSpace}- so I’ve been staring at your profile pic…` },
+                {
+                    list: {
+                        facebookTopElementStyle: 'compact',
+                        options: [
+                            {
+                                title: 'Classic White T-Shirt',
+                                subTitle: 'See all our colors',
+                                imageUrl: 'https://images.fineartamerica.com/images-medium-large/scenic-view-in-canadian-rockies-elena-elisseeva.jpg',
+                                button: {
+                                    title: 'View',
+                                    url: 'https://petersapparel.parseapp.com/view_item?item_id=100',
+                                    facebookWebviewHeight: 'tall'
+                                }
+                            },
+                            {
+                                title: 'Classic White T-Shirt 2',
+                                subTitle: 'See all our colors',
+                                imageUrl: 'https://images.fineartamerica.com/images-medium-large/scenic-view-in-canadian-rockies-elena-elisseeva.jpg',
+                                button: {
+                                    title: 'View',
+                                    action: 'foo',
+                                    facebookWebviewHeight: 'compact'
+                                }
+                            }
+                        ],
+                        standaloneButton: {
+                            title: 'Some title',
+                            url: 'http://google.com'
+                        }
+                    }
+                },
                 { simpleResponses: ['And?', 'Uhh… Okay…'] }
             ];
+            // return [
+            //     { text: `Hey ${userProfile.firstNameWithSpace}- so I’ve been staring at your profile pic…` },
+            //     { simpleResponses: ['And?', 'Uhh… Okay…'] }
+            // ];
         },
         sexy: () => {
-            return [
-                { text: 'You’re sexy!' },
-                { image: userProfile.profilePic, accessibilityText: 'Your user profile image.' },
-                { simpleResponses: ['You’re a creep.', 'You’re a weirdo.'] }
-            ]
+            return [{text: 'You’re sexy!'}, {imageUrl: userProfile.profilePic, accessibilityText: 'Your user profile image.'}, {simpleResponses: ['You’re a creep.', 'You’re a weirdo.']}];
         }
         // template: () => {
         //     return [
@@ -27,5 +57,5 @@ export default function getResponses() {
         //         { simpleResponses: ['One', 'Two'] }
         //     ];
         // }
-    }
+    };
 }
