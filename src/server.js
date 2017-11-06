8ac43fe3e50ae15cd668a67131e04d70bdadcafc
ian <smith.ianrichard@gmail.com>
2017-11-06 10:44:56 -0600

    wip server handler for Facebook

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const facebookAccessToken = JSON.parse(fs.readFileSync(`${__dirname.split('dist'
 
 expressServer.get('/', function (request, response) {
     response.end(JSON.stringify({
-        message: 'It works!  But this service is meant for posts bro.'
+        message: 'It works!  But this service is meant for HTTP posts for Dialogflow.'
     }));
 });
 
@@ -35,24 +35,60 @@ expressServer.post('/', function (request, response) {
     // });
 });
 
-expressServer.post('/facebook_send', function (request, response) {
+expressServer.post('/facebook', function (request, response) {
 
-    outboundRequest({
-        url: `https://graph.facebook.com/v2.6/me/messages?access_token=${facebookAccessToken}`,
-        method: 'POST',
-        headers: [{ name: 'content-type', value: 'application/json' }],
-        json: true,
-        body: {
-            recipient: { id: request.body.userId },
-            message: { text: request.body.message }
-        }
-    }, function (error, outboundResponse, outboundBody) {
-        if (outboundBody && outboundBody.message_id) {
-            response.end(JSON.stringify({message: 'Successfully sent Facebook message'}));
+    response.status(200).send('EVENT_RECEIVED');
+
+    let webhookEvent = request.body.entry[0].messaging[0];
+
+    console.log('post');
+
+    if (webhookEvent.message) {
+        logJsonToFile('original-incoming-facebook-data', request.body);
+        let responseBody = {
+            recipient: { id: webhookEvent.sender.id },
+            message: { text: `You said: ${webhookEvent.message.text}` }
+        };
+        outboundRequest({
+            // url: `https://graph.facebook.com/v2.6/me/messages?access_token=${facebookAccessToken}`,
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {
+                access_token: 'EAABtUCCdPnMBANOabFxzvEQXNjudwIu4UOK6UmveVESZBWymZAr0EfFH9JtVSunG0HeVFgsv5sTWZCYF3bZBnpIemuYVwq5R5OiNUgjp0BhUfdqcEqYEbJxaiJ7c3F3jsCe3hM2gEdmV2BYVwqwrZBtEln1PV8BVA9mn0G34ghQZDZD'
+            },
+            method: 'POST',
+            headers: [{ name: 'content-type', value: 'application/json' }],
+            json: responseBody
+        }, function (error, outboundResponse, outboundBody) {
+            if (outboundBody && outboundBody.message_id) {
+                console.log('success');
+            } else {
+                console.log('fail');
+            }
+        });
+    } else if (webhookEvent.postback) {
+        console.log('postback');
+    }
+});
+
+expressServer.get('/facebook', function (request, response) {
+    console.log('get');
+    let mode = request.query['hub.mode'];
+    let token = request.query['hub.verify_token'];
+
+    if (true !== true) {
+    // if (token !== facebookAccessToken) {
+        response.sendStatus(403);
+    } else {
+        // what Facebook uses to verify the webhook
+        if (mode === 'subscribe') {
+            let challenge = request.query['hub.challenge'];
+            response.status(200).send(challenge);
         } else {
-            response.end(JSON.stringify({message: 'Error sending Facebook message'}));
+            response.end(JSON.stringify({
+                message: 'It works!  But this service is meant for handling HTTP posts for Facebook.'
+            }));
         }
-    });
+    }
 
 });
 
